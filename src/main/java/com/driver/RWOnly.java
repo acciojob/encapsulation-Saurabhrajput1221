61fee41c00821a49f8f08d72d7c17e2897a68d43
Saurabhrajput1221 <95747934+Saurabhrajput1221@users.noreply.github.com>
2022-12-22 12:57:51 +0530

    final commit

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,6 +2,18 @@ package com.driver;
 
 public class RWOnly {
     private String name;
-    
-    
+
+    public void SetName(String name) {
+        this.name = name;
+
+    }
+
+    public  String GetName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        // System.out.println(name);
+        RWOnly n = new RWOnly();
+    }
 }
