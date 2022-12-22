d00932f4a9f1412d566683dba053b986bb6597f4
Saurabhrajput1221 <95747934+Saurabhrajput1221@users.noreply.github.com>
2022-12-22 13:06:51 +0530

    3

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
         RWOnly obj = new RWOnly();
         obj.setName("Honey Singh");
-        System.out.println(obj.getName());
+        obj.getName();
     }
 }
