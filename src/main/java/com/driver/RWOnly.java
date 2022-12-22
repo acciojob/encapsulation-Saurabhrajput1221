d4999b8b887114f53ccbdcce37e64e34fd5dc90a
Saurabhrajput1221 <95747934+Saurabhrajput1221@users.noreply.github.com>
2022-12-22 13:12:14 +0530

    4

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -3,12 +3,12 @@ package com.driver;
 public class RWOnly {
     private String name;
 
-    public void setName((String name) {
+    public void setName(String name) {
         this.name = name;
 
     }
 
-    public  String getName(() {
+    public  String getName() {
         return name;
     }
 
